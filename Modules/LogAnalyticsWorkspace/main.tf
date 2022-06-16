90cca90e08cb1e6e9f82f09c8a4dad57da23553c
sharmaarnav <35055863+sharmaarnav@users.noreply.github.com>
2022-06-17 09:22:02 +1000

    Update main.tf

--- Modules/LogAnalyticsWorkspace/main.tf
+++ Modules/LogAnalyticsWorkspace/main.tf
@@ -2,5 +2,5 @@ resource "azurerm_log_analytics_workspace" "example" {
   name                = var.azurerm_log_analytics_workspace_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = "Free"
+  sku                 = "PerGB2018"
 }
