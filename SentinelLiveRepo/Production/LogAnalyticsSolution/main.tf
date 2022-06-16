23cd8b0a0f6f3d81a6a4b23167921013cefd2550
sharmaarnav <35055863+sharmaarnav@users.noreply.github.com>
2022-06-16 15:43:18 +1000

    s

--- SentinelLiveRepo/Production/LogAnalyticsSolution/main.tf
+++ SentinelLiveRepo/Production/LogAnalyticsSolution/main.tf
@@ -3,8 +3,8 @@ data "azurerm_resource_group" "examplerg" {
 }
 
 
-data "azurerm_log_analytics_solution" "example" {
-  name = var.azurerm_log_analytics_workspace_name
+data "azurerm_log_analytics_workspace" "example" {
+  name = "${var.prefix}-workspace-as01"
   resource_group_name = data.azurerm_resource_group.examplerg.name
 }
 
@@ -13,9 +13,9 @@ module "LogAnalyticsSolution" {
   source = "./LogAnalyticsSolution"
   
   
-  azurerm_log_analytics_solution_name = 
-  location = 
-  resource_group_name = 
-  azurerm_log_analytics_workspace_id = 
-  azurerm_log_analytics_workspace_name = 
+  azurerm_log_analytics_solution_name = "${var.prefix}-solution-as01"
+  location = var.location
+  resource_group_name = data.azurerm_resource_group.examplerg.name
+  azurerm_log_analytics_workspace_id = data.azurerm_log_analytics_workspace.example.id
+  azurerm_log_analytics_workspace_name = data.azurerm_log_analytics_workspace.example.name
 }
