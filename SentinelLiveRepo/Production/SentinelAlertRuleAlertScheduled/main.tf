0ebb65931026a67fc3b3a7226fb20c8f567e4339
sharmaarnav <35055863+sharmaarnav@users.noreply.github.com>
2022-06-17 13:33:35 +1000

    123

--- SentinelLiveRepo/Production/SentinelAlertRuleAlertScheduled/main.tf
+++ SentinelLiveRepo/Production/SentinelAlertRuleAlertScheduled/main.tf
@@ -1,3 +1,15 @@
 data "azurerm_resource_group" "examplerg" {
   name     = "${var.prefix}-rg"
 }
+
+data "azurerm_log_analytics_solution" "example" {
+  name = "${var.prefix}_solutionas01"
+  resource_group_name = data.azurerm_resource_group.examplerg.name
+}
+
+module "SentinelAlertRuleAlertScheduled" {
+  source = "./SentinelAlertRuleAlertScheduled"
+
+azurerm_log_analytics_solution_id = data.azurerm_log_analytics_solution.example.id
+
+}
---
+++ SentinelLiveRepo/Production/SentinelAlertRuleFusion/main.tf
@@ -0,0 +1,15 @@
+data "azurerm_resource_group" "examplerg" {
+  name     = "${var.prefix}-rg"
+}
+
+data "azurerm_log_analytics_solution" "example" {
+  name = "${var.prefix}_solutionas01"
+  resource_group_name = data.azurerm_resource_group.examplerg.name
+}
+
+module "SentinelAlertRuleFusion" {
+  source = "./SentinelAlertRuleFusion"
+
+  azurerm_log_analytics_solution_id = data.azurerm_log_analytics_solution.example.id
+
+}
