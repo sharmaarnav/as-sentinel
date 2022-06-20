44493539c41019cb1a589b042004ae5df4084836
sharmaarnav <35055863+sharmaarnav@users.noreply.github.com>
2022-06-20 11:38:00 +1000

    sd

--- SentinelLiveRepo/Production/SentinelWatchlist/main.tf
+++ SentinelLiveRepo/Production/SentinelWatchlist/main.tf
@@ -16,7 +16,3 @@ module "SentinelWatchlist" {
   azurerm_log_analytics_solution_id = data.azurerm_log_analytics_workspace.example.id
 }
 
-
-output "watchlist_id" {
-  value = module.SentinelWatchlist.id
-}
